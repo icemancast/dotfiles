cc7b475bbd8937ec6814eff1af8c0656c058ef9d
Isaac Castillo <icemancast@gmail.com>
2016-08-03 09:48:21 -0500

    alias files

--- bash/aliases/custom.aliases.bash
+++ bash/aliases/custom.aliases.bash
@@ -1,26 +1,19 @@
 alias subl='/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl'
 alias ll='ls -lahG'
 
-alias create-site="ansible-playbook -i ansible/hosts ansible/site-create.yml -l vagrant"
-alias cs='_create_site'
-alias sc='cs'
+# Macvim inside terminal
 alias mvi='mvim -v'
 
-alias lsc='cd ~/vm; ansible-playbook ansible/playbooks/vagrant/site/php.yml'
-
 # Change directories
-alias .="cd .."
-alias ..="cd ../.."
-alias ...="cd ../../.."
+alias .='cd ..'
+alias ..='cd ../..'
+alias ...='cd ../../..'
 
-alias clients='_cd_site clients'
-alias personal='_cd_site personal $1'
-alias echods='_cd_site echods $1'
-alias codeup="cd ~/Documents/codeup; ll"
-alias vps="cd ~/vm; ll;"
-alias vm="cd ~/vm; ll; pwd"
-alias vl="cd ~/vagrant-lamp; ll; pwd"
+# Misc aliases
+alias projects='_cd_project $1'
+alias homestead='cd ~/Homestead; ll; pwd'
 alias wpt='_cd_wp'
+alias dotfiles='cd ~/dotfiles; mvi .';
 
 # Vagrant commands
 alias vs='vagrant status'
@@ -29,37 +22,33 @@ alias vu='vagrant up'
 alias vh='vagrant halt'
 
 # Git alias
-alias gl="git log --full-diff"
-alias gs="git status"
-alias gco="git co $1"
-alias gc="git commit -m $1"
-alias gm="git merge $1"
+alias gl='git log --full-diff'
+alias gs='git status'
+alias gco='git co $1'
+alias gc='git commit -m $1'
+alias gm='git merge $1'
 
 # SSH alias
-alias ssh-compile="echo -n > ~/.ssh/config && cat ~/.ssh/config.d/* > ~/.ssh/config"
+alias ssh-compile='echo -n > ~/.ssh/config && cat ~/.ssh/config.d/* > ~/.ssh/config'
 
 # Docker commands
-alias dv="_docker_env $1"
-alias dssh="_docker_ssh $1"
+alias dv='_docker_env $1'
+alias dssh='_docker_ssh $1'
 
 # Client specific
-alias evm="cd ~/vm/sites/clients/evermarket/App/EverMarketMobile; ll; pwd"
-alias evm-start="evm; react-native start"
-alias evm-open="evm; open ~/vm/sites/clients/evermarket/App/EverMarketMobile/ios/EverMarketMobile.xcodeproj; mvi ."
-alias evm-web="cd ~/vm/sites/clients/evermarket/EvermarketWeb/Source; sudo http-server -p 443 -S -C ~/vm/sites/clients/evermarket/source_files/cert.pem  -K ~/vm/sites/clients/evermarket/source_files/key.pem"
-alias emv="evm" # Always mispelled
-
-# Variables
-vm_path='/Users/icemancast/vm'
-# SSH alias
+alias evm='cd ~/vm/sites/clients/evermarket/App/EverMarketMobile; ll; pwd'
+alias evm-start='evm; react-native start'
+alias evm-open='evm; open ~/vm/sites/clients/evermarket/App/EverMarketMobile/ios/EverMarketMobile.xcodeproj; mvi .'
+alias evm-web='cd ~/vm/sites/clients/evermarket/EvermarketWeb/Source; sudo http-server -p 443 -S -C ~/vm/sites/clients/evermarket/source_files/cert.pem  -K ~/vm/sites/clients/evermarket/source_files/key.pem'
+alias emv='evm' # Always mispelled
 
 # Alias Functions ============================
 # Enter client site if param exists
-function _cd_site() {
+function _cd_project() {
   if [ "$#" -eq 1 ]; then
-    cd ~/vm/sites/$1;
+    cd ~/projects/$1;
   else
-    cd ~/vm/sites/$1/$2.dev/$3;
+    cd ~/projects/$1/$2;
   fi
   pwd && ll;
 }
@@ -72,14 +61,6 @@ function _cd_wp() {
     cd wp-content/themes/$1;
   fi
   pwd && ll;
-}
-
-# Create site go to vm folder if needed
-function _create_site() {
-  if [ "${PWD}" != $vm_path ]; then
-    cd ~/vm
-  fi
-  create-site
 }
 
 # Start docker machine
