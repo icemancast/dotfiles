a2bd1b29f5855cffa12ce3b2008fb6b04cc4f6b9
Isaac Castillo <icemancast@gmail.com>
2019-11-06 09:00:37 -0600

    changes

--- bash/aliases/clients/mod.bash
+++ bash/aliases/clients/mod.bash
@@ -4,3 +4,9 @@ alias ms='cd ~/develop/mod-labs/mouse_platform'
 alias ms-start='mouse; subl .; gulp watch'
 
 alias mouselogs='scp -r create@165.227.209.188:/srv/users/create/apps/create/storage/logs /Users/icemancast/develop/mod-labs/logs/mouseproduction; scp -r create@165.227.209.188:/srv/users/create/log/create /Users/icemancast/develop/mod-labs/logs/mouseproduction'
+
+alias rilogs='scp -r portal@159.203.162.37:/srv/users/portal/apps/portal/storage/logs /Users/icemancast/develop/mod-labs/logs/riproduction'
+alias ricraftlogs='scp -r serverpilot@45.55.60.27:/srv/users/serverpilot/apps/letsreimagine/storage/logs /Users/icemancast/develop/mod-labs/logs/ricraft'
+
+
+
